6a6d7db460d047d8a9c6bcd2105d0f86e367ad92
Jeremy Weter <jeremyweter@gmail.com>
2026-08-15 09:08:15 -0500

    Bind chord identity history to validated authority

--- src/rocksmith_cdlc_generator/reviewed_chords.py
+++ src/rocksmith_cdlc_generator/reviewed_chords.py
@@ -207,5 +207,9 @@ def set_reviewed_chord_group(
         project,
         kind="chord_identity",
         writes={CHORD_REVIEW_PATH: layer.model_dump_json(indent=2) + "\n"},
+        score_sha256=layer.score_sha256,
+        score_format=layer.score_format,
+        fanout_manifest_path=layer.fanout_manifest_path,
+        fanout_manifest_sha256=layer.fanout_manifest_sha256,
     )
     return layer
