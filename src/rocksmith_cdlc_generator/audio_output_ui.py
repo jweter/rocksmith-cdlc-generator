8ce06e0f144c5823bdd234477dbe8d9657f5186b
Jeremy Weter <jeremyweter@gmail.com>
2026-08-22 21:07:12 -0500

    Wire Product Reality live review enhancements (#379 #380 #382)

--- src/rocksmith_cdlc_generator/audio_output_ui.py
+++ src/rocksmith_cdlc_generator/audio_output_ui.py
@@ -14,6 +14,7 @@ from .audio_output import (
 )
 from .eof_measure_review_ui import EOFMeasureReviewMixin
 from .eof_workspace_ui import EOFWorkspaceMixin
+from .live_review_enhancements import LiveReviewEnhancementMixin
 from .review_queue_workspace_ui import ReviewQueueWorkspaceMixin
 from .score_role_composition_workspace_ui import ScoreRoleCompositionWorkspaceMixin
 from .timing_bpm_workspace_ui import TimingBpmWorkspaceMixin
@@ -21,6 +22,7 @@ from .track_trust_workspace_ui import TrackTrustWorkspaceMixin
 
 
 class AudioOutputSongWorkspaceWindow(
+    LiveReviewEnhancementMixin,
     EOFMeasureReviewMixin,
     EOFWorkspaceMixin,
     ReviewQueueWorkspaceMixin,
